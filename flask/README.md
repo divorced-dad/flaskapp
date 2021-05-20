c4957bfd1bf41ecb0cb775e3dd6a78b2164186f3
Simone Xiao <simone.84858@gmail.com>
2021-05-20 21:42:12 +0200

    t

---
+++ flask/README.md
@@ -0,0 +1,1 @@
+repo initiated exclusively for heroku deploy.
--- flask/flaskapp/config.py
+++ flask/flaskapp/config.py
@@ -4,11 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    user = os.environ['POSTGRES_USER']
-    password = os.environ['POSTGRES_PASSWORD']
-    host = os.environ['POSTGRES_HOST']
-    database = os.environ['POSTGRES_DB']
-    port = os.environ['POSTGRES_PORT']
+    user = os.getenv('POSTGRES_USER', 'postgres')
+    password = os.getenv('POSTGRES_PASSWORD', 'postgres')
+    host = os.getenv('POSTGRES_HOST', 'localhost')
+    database = os.getenv('POSTGRES_DB', 'postgres')
+    port = os.getenv('POSTGRES_PORT', 5432)
 
-    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+    # SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SECRET_KEY = '2a0f51a5c1992df84ee9bbd9817492d5'
